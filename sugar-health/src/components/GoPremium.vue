6f8edf14255d0e095056cfd712be06de7c46db72
csl26 <103038068+csl26@users.noreply.github.com>
2023-09-01 12:25:04 +0200

    added table

--- sugar-health/src/components/GoPremium.vue
+++ sugar-health/src/components/GoPremium.vue
@@ -9,5 +9,99 @@ export default {
 
 <template>
   <h1>Premium</h1>
-  <p> Here is what you get for premium</p>
+  <p> Here is what you get for premium:</p>
+
+  <table>
+  <tr>
+    <th></th>
+    <th>Free</th>
+    <th>Premium</th>
+  </tr>
+  <tr>
+    <td class="condition">Input your suger intake</td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Change your profilepicture</td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Challenges</td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Avatar per profile</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Productsearch</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Add free</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Expanded intake form</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">More than 2 profiles per account</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Statistics</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td class="condition">Pick your own goals</td>
+    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Your current plan</td>
+    <td><button type="button">Go Premium</button></td>
+  </tr>
+</table>
 </template>
+
+<style>
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 75%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: right;
+  padding: 8px;
+}
+
+.condition {
+    text-align: left;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 5px;
+  
+}
+
+</style>
