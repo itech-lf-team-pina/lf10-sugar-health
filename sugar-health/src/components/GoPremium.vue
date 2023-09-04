cc112baf31aa904acfb887f133959a0644351f86
csl26 <103038068+csl26@users.noreply.github.com>
2023-09-04 10:23:39 +0200

    Added styling to table

--- sugar-health/src/components/GoPremium.vue
+++ sugar-health/src/components/GoPremium.vue
@@ -1,94 +1,103 @@
 <script>
 export default {
-  name: 'GoPremium',
-  props: {
-    msg: String
-  }
+    name: 'GoPremium',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <template>
-  <h1>Premium</h1>
-  <p> Here is what you get for premium:</p>
+    <h1>Premium</h1>
+    <p> Here is what you get for premium:</p>
 
-  <table>
-  <tr>
-    <th></th>
-    <th>Free</th>
-    <th>Premium</th>
-  </tr>
-  <tr>
-    <td class="condition">Input your suger intake</td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Change your profilepicture</td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Challenges</td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Avatar per profile</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Productsearch</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Add free</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Expanded intake form</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">More than 2 profiles per account</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Statistics</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td class="condition">Pick your own goals</td>
-    <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
-    <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td>Your current plan</td>
-    <td><button type="button">Go Premium</button></td>
-  </tr>
-</table>
+    <table>
+        <tr>
+            <th></th>
+            <th>Free</th>
+            <th>Premium</th>
+        </tr>
+        <tr>
+            <td class="condition">Input your suger intake</td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Change your profilepicture</td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Challenges</td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Avatar per profile</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Productsearch</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Add free</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Expanded intake form</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">More than 2 profiles per account</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Statistics</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr>
+            <td class="condition">Pick your own goals</td>
+            <td><i class="fa-solid fa-x" style="color: #cb1a1a;"></i></td>
+            <td><i class="fa-solid fa-check" style="color: #51b15b;"></i></td>
+        </tr>
+        <tr class="noBorder">
+            <td></td>
+            <td>Your current plan</td>
+            <td> 3,99€ per month</td>
+        </tr>
+        <tr class="noBorder">
+            <td></td>
+            <td></td>
+            <td><button type="button">Go Premium</button></td>
+        </tr>
+    </table>
 </template>
 
 <style>
-
 table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 75%;
-  margin-left: auto;
-  margin-right: auto;
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 75%;
+    margin-left: auto;
+    margin-right: auto;
 }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: right;
-  padding: 8px;
+td,
+th {
+    border-bottom: 1px solid #dddddd;
+    text-align: right;
+    padding: 8px;
+}
+
+tr.noBorder td{
+    border: 0;
 }
 
 .condition {
@@ -96,12 +105,16 @@ td, th {
 }
 
 button {
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 5px;
-  
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 5px;
+    cursor: pointer;
+
 }
 
+button:hover {
+    background-color: #45a049;
+}
 </style>
