9c9b2328c0afe5dc58ffe62966afe00d549c60c8
csl26 <103038068+csl26@users.noreply.github.com>
2023-09-05 16:51:38 +0200

    fixed typo

--- sugar-health/src/components/SugarHistory.vue
+++ sugar-health/src/components/SugarHistory.vue
@@ -11,6 +11,6 @@ export default {
 <template>
     <h1> Your sugar history </h1>
     <p>
-      This page has a graph showing how much sugar was consumed each day.
+      This page has a graph showing how much sugar was consumed each day. (vue-chart js)
     </p>
   </template>
