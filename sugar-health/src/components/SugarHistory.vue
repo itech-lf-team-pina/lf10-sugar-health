63af9b144f6a6207bcedeac1cb64a8679dda0509
csl26 <103038068+csl26@users.noreply.github.com>
2023-09-11 13:15:54 +0200

    Start on get method on sugarHistory

--- sugar-health/src/components/SugarHistory.vue
+++ sugar-health/src/components/SugarHistory.vue
@@ -1,8 +1,37 @@
 <script>
+import { BACKEND_URL } from "./baseUrl"
+
 export default {
   name: 'sugarHistory',
   props: {
     msg: String
+  },
+  methods: {
+    async getSugarDataFromServer() {
+      
+      console.log("From local Storage: " + localStorage.getItem("memberId"))
+      const memberID = localStorage.getItem("memberId"); 
+      try {
+        const response = await fetch(`${BACKEND_URL}/${memberID}//sugar/`, {
+          method: "GET",
+          headers: {
+          },
+          
+        });
+
+        if (response.ok) {
+          const data = await response.json(); // Read the response as JSON
+          console.log(data.memberID);
+        } else {
+          // Handle error
+          console.error("Error sending data to the server.");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    }
+
+
   }
 }
 </script>
@@ -13,4 +42,5 @@ export default {
     <p>
       This page has a graph showing how much sugar was consumed each day. (vue-chart js)
     </p>
+   
   </template>
